2108c93d0141992db890d3b9cf399a6a0ad47391
Deyser Orozco <deiser1999@gmail.com>
2021-09-06 09:12:00 -0500

    :zap: Fixed bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  serverSocket: 'http://192.168.1.3:3000',
-  // serverSocket: 'https://alerts-heippi.herokuapp.com/',
+  // serverSocket: 'http://192.168.1.3:3000',
+  serverSocket: 'https://alerts-heippi.herokuapp.com/',
   vapidPublicKey: 'BLpWrYjfdkphUVTEOlTjKg3InCo99o2-5cvLqiPZ83I6H0Djac-gvXW6AkKrQzXTxp0MEnZUa4GijosGoiKQYJc',
   firebaseConfig: {
     apiKey: "AIzaSyApp_sv83eN7GTP9mjGAlgnP5Zywsv-FOU",
