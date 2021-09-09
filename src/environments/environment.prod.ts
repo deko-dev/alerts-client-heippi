2e3bc71c80bf7a855e3e89dd55b1b9e90d928025
Deyser Orozco <deiser1999@gmail.com>
2021-09-09 16:00:56 -0500

    :zap: Change firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,14 @@ export const environment = {
   production: true,
   serverSocket: 'https://alerts-heippi.herokuapp.com/',
   vapidPublicKey: 'BLpWrYjfdkphUVTEOlTjKg3InCo99o2-5cvLqiPZ83I6H0Djac-gvXW6AkKrQzXTxp0MEnZUa4GijosGoiKQYJc',
-  firebaseConfig: {
-    apiKey: "AIzaSyApp_sv83eN7GTP9mjGAlgnP5Zywsv-FOU",
-    authDomain: "yohu-f1341.firebaseapp.com",
-    projectId: "yohu-f1341",
-    storageBucket: "yohu-f1341.appspot.com",
-    messagingSenderId: "9915870085",
-    appId: "1:9915870085:web:6fb0d87879a001a0afb2ec"
-  }
+  firebaseConfig = {
+    apiKey: "AIzaSyCEwOLd6nEb6HKP8U00V9K8LxN5UEvtTho",
+    authDomain: "yohu-co.firebaseapp.com",
+    projectId: "yohu-co",
+    storageBucket: "yohu-co.appspot.com",
+    messagingSenderId: "315119656949",
+    appId: "1:315119656949:web:87900a4427ffb4c4da4abc",
+    measurementId: "G-DQ3HB0RGF0"
+  };
 };
 
